0dd88f078049aae5127c82000bb3477c3881d358
chrisgarceau <49497694+chrisgarceau@users.noreply.github.com>
2023-04-02 16:30:19 -0400

    Update main.py

--- capML/main.py
+++ capML/main.py
@@ -6,16 +6,24 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
 from keras.callbacks import EarlyStopping
 import matplotlib.pyplot as plt
+from datetime import datetime as dt
+import pygsheets
 
+
+# FETCHING SPREADSHEET/CSV FROM GOOGLE SHEETS
+SHEET_ID = '1Q1O_vjp0bDg3PRkkk224PkvbLb02kclj5ltx32WcPhQ'
+SHEET_NAME = 'Sheet1'
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 
 # DATA PREPROCESSING
 # ------------------
 # Read the CSV file
-df = pd.read_csv('mock.csv')
+df = pd.read_csv(url)
 # Drop the 'car_enter' and 'car_exit' columns
 df = df.drop(['car_enter', 'car_exit'], axis=1)
 # Combine the 'date' and 'time' columns into a single column
 df['datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+df['weekday'] = df['datetime'].dt.day_of_week
 # Drop the 'date' and 'time' columns
 df = df.drop(['date', 'time'], axis=1)
 df = df.set_index('datetime')
@@ -25,11 +33,19 @@ df['total_cars'] = pd.to_numeric(df['total_cars'])
 df = df.astype('float32')
 
 
+# CREATE NEW DATAFRAME BY EXTRACTING ALL DATA FOR CURRENT DAY OF WEEK
+dt = dt.now()
+currWeekday = dt.weekday();
+print(dt.weekday())
+grouped = df.groupby(df.weekday)
+current_dow = grouped.get_group(currWeekday)
+current_dow = current_dow.drop(['weekday'], axis=1)
+
 # SPLIT THE DATA INTO TRAINING AND TESTING SETS
 # ---------------------------------------------
 #   - Training set consists of 80% of the dataset
 #   - Test set consists of 20% of the dataset
-train, test = train_test_split(df, test_size=0.20, shuffle=False)
+train, test = train_test_split(current_dow, test_size=0.20, shuffle=False)
 
 
 # NORMALIZE THE TRAINING AND TEST SETS
@@ -115,49 +131,64 @@ y_test_inv = scaler.inverse_transform(y_test.reshape(-1, 1))
 y_test = scaler.inverse_transform(y_test.reshape(-1, 1))
 predictions = scaler.inverse_transform(predictions)
 
-# 1. REAL + PREDICTED: TEST DATA (last 20 % of the dataset)
-# Plot the actual values and the predicted values
-plt.plot(df.index.values[-len(y_test):], y_test, label='Actual')
-plt.plot(df.index.values[-len(predictions):], predictions, label='Predicted')
-plt.legend(loc='upper right')
-plt.xlabel('Datetime')
-plt.ylabel('Total Cars')
-plt.legend()
-plt.show()
-plt.close()
+# # 1. REAL + PREDICTED: TEST DATA (last 20 % of the dataset)
+# # Plot the actual values and the predicted values
+# plt.plot(current_dow.index.values[-len(y_test):], y_test, label='Actual')
+# plt.plot(current_dow.index.values[-len(predictions):], predictions, label='Predicted')
+# plt.legend(loc='upper right')
+# plt.xlabel('Datetime')
+# plt.ylabel('Total Cars')
+# plt.legend()
+# plt.show()
+# plt.close()
 
-# 2. REAL + PREDICTED (overlay)
-# Plot the predicted and actual values
-plt.plot(df.index, df['total_cars'], label='Actual')
-plt.plot(df.index[-len(y_test):], y_pred_inv, label='Predicted')
-plt.xlabel('Datetime')
-plt.ylabel('Total Cars')
-plt.legend(loc='upper right')
-plt.show()
-plt.close()
+# # 2. REAL + PREDICTED (overlay)
+# # Plot the predicted and actual values
+# plt.plot(current_dow.index, current_dow['total_cars'], label='Actual')
+# plt.plot(current_dow.index[-len(y_test):], y_pred_inv, label='Predicted')
+# plt.xlabel('Datetime')
+# plt.ylabel('Total Cars')
+# plt.legend(loc='upper right')
+# plt.show()
+# plt.close()
 
-# 3. REAL + PREDICTED
-plt.plot(df.iloc[:(int(len(df) * 0.8)), -1], label='Actual')
-plt.plot(df.index[-len(y_test):], y_pred_inv, label='Predicted')
-plt.legend(loc='upper right')
-plt.xlabel('Datetime')
-plt.ylabel('Total Cars')
-plt.show()
+# # 3. REAL + PREDICTED
+# plt.plot(current_dow.iloc[:(int(len(current_dow) * 0.8)), -1], label='Actual')
+# plt.plot(current_dow.index[-len(y_test):], y_pred_inv, label='Predicted')
+# plt.legend(loc='upper right')
+# plt.xlabel('Datetime')
+# plt.ylabel('Total Cars')
+# plt.show()
 
 
 # PRINTING/STORING REAL & PREDICTED VALUES
 # ----------------------------------------
 results = pd.DataFrame({'actual': y_test.reshape(-1), 'predicted': predictions.reshape(-1)})
 # Add a datetime column to the results dataframe
-dates = df.index[-len(y_test):]
+dates = current_dow.index[-len(y_test):]
 results = pd.DataFrame(data=results, columns=['actual', 'predicted'])
 results['datetime'] = dates
 results = results.set_index(results['datetime'])
 results = results.drop(['datetime'], axis=1)
 
 
+# Resample the dataframe at 30-minute intervals and calculate the mean of the total number of cars
+# This is necessary for plotting data points in the frontend/mobile application
+results_resampled = results.resample('31T').mean()
+results_resampled = results_resampled.reset_index()
+# Print resampled dataframe
+# print(results_resampled)
 # Print the dataframe
-print(results)
+# print(results)
 
 # Store values in .CSV file
-results.to_csv('results.csv', index=True)
+results_resampled.to_csv('results.csv', index=True)
+
+# Exporting ML results to Google Sheet
+# authorization
+gc = pygsheets.authorize(service_file='/Users/christopher_garceau/crested-century-368601-3cb00067a757.json')
+sh = gc.open('Google Spread Sheet API')
+# select the third sheet 
+wks = sh[2]
+# update the third sheet with df, starting at cell A1. 
+wks.set_dataframe(results_resampled,(0,0))
